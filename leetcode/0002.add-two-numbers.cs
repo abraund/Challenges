ea39f054d4716b6324215a59f52e4fc4dc8f9b99
Anthony Braund <abraund@users.noreply.github.com>
2024-05-31 15:06:31 +0100

    Written on phone, and yet worked well enough.

---
+++ leetcode/0002.add-two-numbers.cs
@@ -0,0 +1,32 @@
+﻿namespace Challenges.leetcode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode? AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return Recurse(l1, l2, 0);
+        }
+
+        public ListNode? Recurse(ListNode? l1, ListNode? l2, int carry)
+        {
+            if (l1 == null && l2 == null && carry == 0) 
+                return null;
+
+            var res = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+            var next = Recurse(l1?.next, l2?.next, res / 10);
+            return new ListNode((res % 10), next);
+        }
+    }
+}
