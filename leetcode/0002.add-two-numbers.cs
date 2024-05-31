2ebafcae1c736cc1eb1625b5f9d7c09f38efdc17
Anthony Braund <abraund@users.noreply.github.com>
2024-05-31 18:00:44 +0100

    A better namespace when leetcode will spam Solution classes.

--- leetcode/0002.add-two-numbers.cs
+++ leetcode/0002.add-two-numbers.cs
@@ -1,32 +1,32 @@
-﻿namespace Challenges.leetcode
-{
-    public class ListNode
-    {
-        public int val;
-        public ListNode? next;
+﻿
+namespace _0002;
 
-        public ListNode(int val = 0, ListNode? next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
+public class ListNode
+{
+    public int val;
+    public ListNode? next;
+
+    public ListNode(int val = 0, ListNode? next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode? AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        return Recurse(l1, l2, 0);
     }
 
-    public class Solution
+    public ListNode? Recurse(ListNode? l1, ListNode? l2, int carry)
     {
-        public ListNode? AddTwoNumbers(ListNode l1, ListNode l2)
-        {
-            return Recurse(l1, l2, 0);
-        }
+        if (l1 == null && l2 == null && carry == 0) 
+            return null;
 
-        public ListNode? Recurse(ListNode? l1, ListNode? l2, int carry)
-        {
-            if (l1 == null && l2 == null && carry == 0) 
-                return null;
-
-            var res = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
-            var next = Recurse(l1?.next, l2?.next, res / 10);
-            return new ListNode((res % 10), next);
-        }
+        var res = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+        var next = Recurse(l1?.next, l2?.next, res / 10);
+        return new ListNode((res % 10), next);
     }
 }
