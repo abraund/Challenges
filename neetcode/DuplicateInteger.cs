75922d13ac13392e8d3503b175f9e73292c77db5
Anthony Braund <abraund@users.noreply.github.com>
2024-08-07 07:27:11 +0100

    Neetcode duplicate integer.

---
+++ neetcode/DuplicateInteger.cs
@@ -0,0 +1,40 @@
+namespace neetcode
+{
+    public class DuplicateInteger
+    {
+
+        public bool HasDuplicate(int[] nums)
+        {
+            var hash = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (hash.Contains(num))
+                    return true;
+                hash.Add(num);
+            }
+
+            return false;
+        }
+
+        [Fact]
+        public void AscendingNumbersWithDuplicate()
+        {
+            var nums = new[] { 1, 2, 3, 3 };
+            Assert.True(HasDuplicate(nums));
+        }
+
+        [Fact]
+        public void MixedNumbersNoDuplicate()
+        {
+            var nums = new[] { 1, 2, 3, 4 };
+            Assert.False(HasDuplicate(nums));
+        }
+
+        [Fact]
+        public void MixedNumbersWithDuplicate()
+        {
+            var nums = new[] { 1, 2, 3, 3 };
+            Assert.True(HasDuplicate(nums));
+        }
+    }
+}
