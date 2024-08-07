b14df170b63a08f71391fa319671db7fbab79427
Anthony Braund <abraund@users.noreply.github.com>
2024-08-07 07:27:57 +0100

    I'll assume I added this test with good reason.

--- leetcode/0001.two-sum.ts
+++ leetcode/0001.two-sum.ts
@@ -1,15 +1,20 @@
-console.log('result: [' + twoSum([1,1,20,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,80,1,1,1,1,1,1,1,1,1,1], 100) + '], expected: [4,5]');
-
 function twoSum(nums: number[], target: number): number[] {
     let hash: Record<number, number> = {}
-    for(let i = 0; i < nums.length; i++)
-    {
+    for (let i = 0; i < nums.length; i++) {
         let current = nums[i];
         let partner = target - current;
         let partnerIndex = hash[partner];
-        if(partnerIndex !== undefined)
+        if (partnerIndex !== undefined)
             return [partnerIndex, i];
         hash[nums[i]] = i;
     }
     throw new Error("cannot get here");
 };
+
+describe('two sum', () => {
+    test('find complements that add to 100', () => {
+        let x = twoSum([1, 2, 20, 4, 5, 6, 7, 8, 9, 10, 80, 12, 13, 14, 15], 100);
+        expect(x[0]).toBe(2)
+        expect(x[1]).toBe(10)
+    });
+});
