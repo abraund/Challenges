3c9d51b591b2fb26e3f76ab98f68049432395f66
Anthony Braund <abraund@users.noreply.github.com>
2024-08-08 16:09:52 +0100

    Rewritten to use a custom hash function. XOR works better than addition. With unchecked addition I couldn't get ac not to equal ab on hash production.
    No idea really if this is a good hash function. One could test by making random strings and seeing how often collisions happen perhaps.

--- neetcode/AnagramGroups.cs
+++ neetcode/AnagramGroups.cs
@@ -2,27 +2,77 @@ using System.Diagnostics;
 
 namespace neetcode
 {
+    public class StringWrap
+    {
+        public string Value { get; set; }
+
+        public StringWrap(string value)
+        {
+            Value = value;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            foreach (var chr in Value)
+            {
+                hash = hash ^ chr.GetHashCode(); 
+            }
+            return hash;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return IsAnagramPerf(((StringWrap)obj).Value, Value);
+        }
+
+        public bool IsAnagramPerf(string s, string t)
+        {
+            // O(s+t)
+            var charCount = new Dictionary<char, int>();
+            foreach (var c in s)
+            {
+                if (charCount.ContainsKey(c))
+                    charCount[c]++;
+                else
+                    charCount[c] = 1;
+            }
+
+            foreach (var c in t)
+            {
+                if (charCount.ContainsKey(c))
+                    charCount[c]--;
+                else
+                    return false;
+            }
+
+            foreach (var c in charCount)
+            {
+                if (c.Value != 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static implicit operator StringWrap(string str) => new StringWrap(str);
+    }
+
     public class AnagramGroups
     {
         public List<List<string>> GroupAnagrams(string[] strs)
         {
-            var baseAnagramToWord = new Dictionary<string, List<string>>();
+            var baseAnagramToWord = new Dictionary<StringWrap, List<string>>();
             foreach (var str in strs)
             {
-                var sorted = string.Join("", str.OrderBy(x => x));
-                if (baseAnagramToWord.TryGetValue(sorted, out var innerList))
-                {
+                if (baseAnagramToWord.TryGetValue(str, out var innerList))
                     innerList.Add(str);
-                }
                 else
-                {
-                    baseAnagramToWord[sorted] = new List<string>(){str};
-                }
+                    baseAnagramToWord[str] = new List<string>(){str};
             }
 
             return baseAnagramToWord.Select(x => x.Value).ToList();
         }
-        
 
         [Fact]
         public void AnagramGroupsTest()
@@ -31,7 +81,33 @@ namespace neetcode
             var result = GroupAnagrams(input);
             Assert.Equal("act", result[0][0]);
             Assert.Equal("cat", result[0][1]);
+        }
 
+        [Fact]
+        public void Test()
+        {
+            var act = new StringWrap("act");
+            var cat = new StringWrap("cat");
+
+            Assert.Equal(act.GetHashCode(), cat.GetHashCode());
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var act = new StringWrap("eeacet").GetHashCode();
+            var cat = new StringWrap("cateee").GetHashCode();
+
+            Assert.Equal(act.GetHashCode(), cat.GetHashCode());
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var ac = new StringWrap("ac").GetHashCode();
+            var bb = new StringWrap("bb").GetHashCode();
+
+            Assert.NotEqual(ac.GetHashCode(), bb.GetHashCode());
         }
     }
 }
