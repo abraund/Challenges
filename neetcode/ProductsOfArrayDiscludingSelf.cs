2068018394f88cb3fd980f69ec892e4efdeb1aca
Anthony Braund <abraund@users.noreply.github.com>
2024-08-11 16:49:12 +0100

    Hmm, I did need to find inspiration for an idea, if not the implementation. The code comes out rather nicely on the plus side, no shimmying to deal with out by 1 sort of problems.

---
+++ neetcode/ProductsOfArrayDiscludingSelf.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+
+namespace neetcode
+{
+    public class ProductsOfArrayDiscludingSelf
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var result  = new int[nums.Length];
+
+            var product = 1;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                result[i] = product;
+                product *= nums[i];
+            }
+
+            product = 1;
+            for (var i = nums.Length - 1; i >= 0; i--)
+            {
+                result[i] = result[i] * product;
+                product *= nums[i];
+            }
+            return result;
+        }
+
+        [Fact]
+        public void Ascending()
+        {
+            var result = ProductExceptSelf([1, 2, 4, 6]);
+            Assert.Equal([48, 24, 12, 8], result);
+        }
+
+        [Fact]
+        public void ContainsZero()
+        {
+            var result = ProductExceptSelf([-1, 0, 1, 2, 3]);
+            Assert.Equal([0, -6, 0, 0, 0], result);
+        }
+    }
+}
