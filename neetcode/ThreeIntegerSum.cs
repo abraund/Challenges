ada5150355af1cc5ac0882e8a76e72ab2180aaca
Anthony Braund <abraund@users.noreply.github.com>
2024-08-12 20:16:35 +0100

    Pfft, took an age to write this. Tried all sorts of abortive attempts, inc hashing all the pairs (which I think did work, but I wouldn't accept sorting my results to avoid collisions at the time).
    The double loop, and the check that I'm always searching forward (positions in the dictionary) ensures no double/triple use of the same value. Kind of interesting.

---
+++ neetcode/ThreeIntegerSum.cs
@@ -0,0 +1,49 @@
+using System.Diagnostics;
+
+namespace neetcode;
+
+public class ThreeIntegerSum
+{
+    public List<List<int>> ThreeSum(int[] nums)
+    {
+        var result = new HashSet<(int i, int j, int k)>();
+
+        var valueToPos = new Dictionary<int, List<int>>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (valueToPos.TryGetValue(nums[i], out var value))
+                value.Add(i);
+            else
+                valueToPos[nums[i]] = new List<int>(){i};
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var iVal = nums[i];
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                var jVal = nums[j];
+                var kVal = 0 - iVal - jVal;
+                if (valueToPos.TryGetValue(kVal, out var kPositions))
+                {
+                    if (kPositions.Any(k => k > j))
+                    {
+                        var tupleArray = new[] { iVal, jVal, kVal };
+                        Array.Sort(tupleArray);
+                        result.Add((tupleArray[0], tupleArray[1], tupleArray[2]));
+                    }
+                }
+            }
+        }
+
+        return result.Select(x => new int[]{x.i, x.j, x.k}.ToList()).ToList();
+    }
+
+    [Fact]
+    public void IncNumber()
+    {
+        var result = ThreeSum([-1, 0, 1, 2, -1, -4]);
+        Assert.Equal([[-1, 0, 1], [-1, -1, 2]], result);
+    }
+}
