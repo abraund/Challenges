dcd3f5eefded4f31d210ce0ed14840b1fd71c875
Anthony Braund <abraund@users.noreply.github.com>
2024-08-12 19:47:58 +0100

    Look forward and backward at the same time, need to sometimes skip to dodge non-alphanumeric characters.
    Checking other solutions I note they while rather than if and continue, I think that solution strictly better - it more strongly emphasises the purpose, the flow causes by the continue is less clear.

---
+++ neetcode/ValidPalindrome.cs
@@ -0,0 +1,101 @@
+using System.Diagnostics;
+
+namespace neetcode
+{
+    public class ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            var i = 0;
+            var j = s.Length - 1;
+
+            while (i < j)
+            {
+                var iChar = s[i];
+                if (!char.IsLetterOrDigit(iChar))
+                {
+                    i++;
+                    continue;
+                }
+
+                var jChar = s[j];
+                if (!char.IsLetterOrDigit(jChar))
+                {
+                    j--;
+                    continue;
+                }
+
+                if (!string.Equals(iChar.ToString(), jChar.ToString(), StringComparison.OrdinalIgnoreCase))
+                    return false;
+
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+
+        [Fact]
+        public void IncNumber()
+        {
+            var result = IsPalindrome("0P");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void SmallWithSpecial()
+        {
+            var result = IsPalindrome("?P");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void SomehowWrongPalindrome()
+        {
+            var result = IsPalindrome("ab");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void OddPalindrome()
+        {
+            var result = IsPalindrome("tat");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void EvenPalindrome()
+        {
+            var result = IsPalindrome("taat");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void OddPalindromeWithSpecialChar()
+        {
+            var result = IsPalindrome("tat?");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void EvenPalindromeWithSpecialChar()
+        {
+            var result = IsPalindrome("taat?");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Palindrome()
+        {
+            var result = IsPalindrome("Was it a car or a cat I saw?");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void NotPalindrome()
+        {
+            var result = IsPalindrome("tab a cat");
+            Assert.False(result);
+        }
+    }
+}
