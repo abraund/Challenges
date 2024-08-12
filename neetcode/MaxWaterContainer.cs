b0efc3781f2c86acc8571ebdb66363d639a319c9
Anthony Braund <abraund@users.noreply.github.com>
2024-08-12 23:44:26 +0100

    It's noticable better solutions always easier to code, O(n). Had to find inspiration for this one hmm. Should have found this one.

--- neetcode/MaxWaterContainer.cs
+++ neetcode/MaxWaterContainer.cs
@@ -26,12 +26,36 @@ public class MaxWaterContainer
         return maxArea;
     }
 
+    public int MaxAreaShiftWhicheverSmaller(int[] heights)
+    {
+        var maxArea = 0;
 
+        var left = 0;
+        var right = heights.Length - 1;
+
+        while (left < right)
+        {
+            var leftHeight = heights[left];
+            var rightHeight = heights[right];
+            var area = Math.Min(leftHeight, rightHeight) * (right - left);
+            maxArea = Math.Max(maxArea, area);
+            if (leftHeight < rightHeight)
+                left++;
+            else
+                right--;
+        }
+
+        return maxArea;
+    }
+    
     [Fact]
     public void Test1()
     {
         var result = MaxArea([1, 7, 2, 5, 4, 7, 3, 6]);
         Assert.Equal(36, result);
+        var result2 = MaxAreaShiftWhicheverSmaller([1, 7, 2, 5, 4, 7, 3, 6]);
+        Assert.Equal(36, result2);
+
     }
 
     [Fact]
@@ -39,6 +63,8 @@ public class MaxWaterContainer
     {
         var result = MaxArea([2, 2, 2]);
         Assert.Equal(4, result);
+        var result2 = MaxAreaShiftWhicheverSmaller([2, 2, 2]);
+        Assert.Equal(4, result2);
     }
 
 }
