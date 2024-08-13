862c2a3f71e94fe335f959c920211e14e9b4c640
Anthony Braund <abraund@users.noreply.github.com>
2024-08-13 20:14:09 +0100

    Max profit sliding window style, hmm. I've got it in the head logically but not intuitively, the closest I can get intuitively is that my right search should be greedy, I'm low on the left, but there may be more to find if I keep hunting right. If I find something lower, just a chance for more greed. A new low point from which to explore onwards.
    Not wholly in love with my increment logic. Will check an implementation thust to see if there are cleaner efforts.

--- neetcode/BuyAndSellCrypto.cs
+++ neetcode/BuyAndSellCrypto.cs
@@ -20,22 +20,47 @@ public class BuyAndSellCrypto
         return maxProfit;
     }
 
+    public int MaxProfitSlidingWindow(int[] prices)
+    {
+        var maxProfit = 0;
+
+        var i = 0;
+        var j = 1;
+
+        while (i < prices.Length - 1)
+        {
+            var left = prices[i];
+            var right = prices[j];
+
+            maxProfit = Math.Max(maxProfit, right - left);
+
+            if (left > right || j == prices.Length - 1)
+                i++;
+            else if (left <= right)
+                j++;
+        }
+
+        return maxProfit;
+    }
+
     [Fact]
     public void Test1()
     {
         Assert.Equal(6, MaxProfit([10, 1, 5, 6, 7, 1]));
+        Assert.Equal(6, MaxProfitSlidingWindow([10, 1, 5, 6, 7, 1]));
     }
 
     [Fact]
     public void Test2()
     {
         Assert.Equal(0, MaxProfit([10, 8, 7, 5, 2]));
+        Assert.Equal(0, MaxProfitSlidingWindow([10, 8, 7, 5, 2]));
     }
 
     [Fact]
     public void Test3()
     {
         Assert.Equal(6, MaxProfit([1, 7, 1]));
+        Assert.Equal(6, MaxProfitSlidingWindow([1, 7, 1]));
     }
-
 }
