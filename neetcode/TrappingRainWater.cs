e54e918711fcfad4da0b17fb3fccc111f0082a9d
Anthony Braund <abraund@users.noreply.github.com>
2024-08-13 01:08:42 +0100

    Interesting solution, the idea is that if you have a peak on the left, and a peak on the right, and say you are on a column where water could go between those peaks, well then one way or the other it will.

--- neetcode/TrappingRainWater.cs
+++ neetcode/TrappingRainWater.cs
@@ -28,10 +28,46 @@ public class TrappingRainWater
         return totalWater;
     }
 
+    public int Trap2(int[] heights)
+    {
+        var totalWater = 0;
+
+        var left = 0;
+        var right = heights.Length - 1;
+        var i = 0;
+
+        var maxHeightLeft = 0;
+        var maxHeightRight = 0;
+
+        while (left < right)
+        {
+            maxHeightLeft = Math.Max(maxHeightLeft, heights[left]);
+            maxHeightRight = Math.Max(maxHeightRight, heights[right]);
+
+            totalWater += Math.Max(Math.Min(maxHeightLeft, maxHeightRight) - heights[i], 0);
+
+            if (maxHeightLeft < maxHeightRight)
+            {
+                left++;
+                i = left;
+            }
+            else
+            {
+                right--;
+                i = right;
+            }
+        }
+
+        return totalWater;
+    }
+
+
     [Fact]
     public void TrapTest()
     {
         var result = Trap([0, 2, 0, 3, 1, 0, 1, 3, 2, 1]);
         Assert.Equal(9, result);
+        var result2 = Trap2([0, 2, 0, 3, 1, 0, 1, 3, 2, 1]);
+        Assert.Equal(9, result2);
     }
 }
