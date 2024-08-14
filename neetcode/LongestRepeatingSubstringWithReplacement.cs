23d3ca1bf5cee9755746b2bfc914fe2380086a07
Anthony Braund <abraund@users.noreply.github.com>
2024-08-14 20:17:29 +0100

    My original effort, it almost worked, a complicated foray of tracking maxChar & maxCharCount, but it fell apart when I realised I had to decrement the maxCharCount sometimes and would have to search the charCount hash for the new max char. With that in mind I pivoted to just checking finding the max char fresh on each iteration.
    This was somewhat disappointing, so I asked ChatGPT to fix it, but you can't fix what doesn't work. Instead it threw CharacterReplacement2 at me, where my only comment is that maxIndificualCharCount can be wrong, something about the way the window contracts deals with the fact the var can live in error. I tried tripping it up with test cases on realising it might be wrong, but it seems fine. Need to analyse this piece of genius further. Sigh. The AIs are getting good.
    
    I should probably swap the dictiionary for an array, but whichever.

---
+++ neetcode/LongestRepeatingSubstringWithReplacement.cs
@@ -0,0 +1,94 @@
+namespace neetcode;
+
+public class LongestRepeatingSubstringWithReplacement
+{
+    public int CharacterReplacement(string s, int k)
+    {
+        var winningSubstringLength = 0;
+        var left = 0;
+        var right = 0;
+        var charToCountInWindow = Enumerable.Range(0, 26)
+            .Select(x => (char)('A' + x))
+            .ToDictionary(key => key, val => 0);
+
+        while (right < s.Length)
+        {
+            charToCountInWindow[s[right]]++;
+            var windowSize = right - left + 1;
+            var maxChars = charToCountInWindow.Values.Max();
+
+            if (windowSize > k + maxChars)
+            {
+                charToCountInWindow[s[left]]--;
+                left++;
+            }
+            else
+            {
+                winningSubstringLength = Math.Max(winningSubstringLength, windowSize);
+            }
+            right++;
+        }
+
+        return winningSubstringLength;
+    }
+
+    public int CharacterReplacement2(string s, int k)
+    {
+        var winningSubstringLength = 0;
+        var left = 0;
+        var right = 0;
+        var charToCountInWindow = Enumerable.Range(0, 26)
+            .Select(x => (char)('A' + x))
+            .ToDictionary(key => key, val => 0);
+        var maxIndividualCharCount = 0; // can be wrong
+
+        while (right < s.Length)
+        {
+            charToCountInWindow[s[right]]++;
+            maxIndividualCharCount = Math.Max(maxIndividualCharCount, charToCountInWindow[s[right]]);
+
+            int windowSize = right - left + 1;
+            if (windowSize > k + maxIndividualCharCount)
+            {
+                charToCountInWindow[s[left]]--;
+                left++;
+            }
+            else
+            {
+                winningSubstringLength = Math.Max(winningSubstringLength, windowSize);
+            }
+
+            right++;
+        }
+
+        return winningSubstringLength;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(4, CharacterReplacement("XYYX", k: 2));
+        Assert.Equal(4, CharacterReplacement2("XYYX", k: 2));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        Assert.Equal(5, CharacterReplacement("AAABABB", k: 1));
+        Assert.Equal(5, CharacterReplacement2("AAABABB", k: 1));
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        Assert.Equal(3, CharacterReplacement("BAAA", k: 0));
+        Assert.Equal(3, CharacterReplacement2("BAAA", k: 0));
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        Assert.Equal(5, CharacterReplacement("AABCBCBBCBB", k: 1));
+        Assert.Equal(5, CharacterReplacement2("AABCBCBBCBB", k: 1));
+    }
+}
