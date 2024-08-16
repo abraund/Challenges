7801154aacb3a3a06c6f237fe0eb62d2cd50721b
Anthony Braund <abraund@users.noreply.github.com>
2024-08-16 21:52:24 +0100

    Passes but more perf available, not sure.

--- leetcode/0994.rotting-orange.cs
+++ leetcode/0994.rotting-orange.cs
@@ -1,136 +1,106 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Challenges
+namespace Challenges;
+
+internal class _0994
 {
-    internal class _0994
+    public int OrangesRotting(int[][] grid)
     {
-        public int OrangesRotting(int[][] grid)
+        var rotters = new HashSet<(int Row, int Column)>();
+        var links = new Dictionary<(int Row, int Col),List<(int Row, int Col)>>();
+        var fresh = 0;
+
+        for (int row = 0; row < grid.Length; row++)
         {
-            var rotters = new HashSet<(int Row, int Column)>();
-            var links = new Dictionary<(int Row, int Col),List<(int Row, int Col)>>();
-            var fresh = 0;
-
-            for (int row = 0; row < grid.Length; row++)
+            for (int col = 0; col < grid[row].Length; col++)
             {
-                for (int col = 0; col < grid[row].Length; col++)
+                var slot = grid[row][col];
+
+                if(slot == 0)
+                    continue;
+
+                if (slot == 1)
+                    fresh++;
+
+                if (slot == 2)
+                    rotters.Add((row, col));
+
+                if (!links.ContainsKey((row, col)))
+                    links[(row, col)] = new List<(int Row, int Col)>();
+
+                if (col + 1 < grid[row].Length && grid[row][col + 1] != 0)
                 {
-                    var slot = grid[row][col];
+                    links[(row, col)].Add((row, col + 1));
+                    if(links.TryGetValue((row, col + 1), out var list))
+                        list.Add((row, col));
+                    else
+                        links[(row, col + 1)] = new List<(int Row, int Col)>() { (row, col) };
+                }
 
-                    if(slot == 0)
-                        continue;
+                if (row + 1 < grid.Length && grid[row + 1][col] != 0)
+                {
+                    links[(row, col)].Add((row + 1, col));
+                    if (links.TryGetValue((row + 1, col), out var list))
+                        list.Add((row, col));
+                    else
+                        links[(row + 1, col)] = new List<(int Row, int Col)>() { (row, col) };
+                }
+            }
+        }
 
-                    if (slot == 1)
-                        fresh++;
+        var iterations = 0;
+        var stateChange = false;
+        do
+        {
+            stateChange = false;
+            var newVictims = new HashSet<(int Row, int Column)>();
 
-                    if (slot == 2)
-                        rotters.Add((row, col));
-
-                    if (!links.ContainsKey((row, col)))
-                        links[(row, col)] = new List<(int Row, int Col)>();
-
-                    if (col + 1 < grid[row].Length && grid[row][col + 1] != 0)
-                    {
-                        if (links.TryGetValue((row, col), out var list))
-                        {
-                            list.Add((row, col + 1));
-                            if(links.TryGetValue((row, col + 1), out list))
-                                list.Add((row, col));
-                            else
-                                links[(row, col + 1)] = new List<(int Row, int Col)>() { (row, col) };
-                        }
-                        else
-                        {
-                            links[(row, col)] = new List<(int Row, int Col)>() { (row, col + 1) };
-                            if (links.TryGetValue((row, col + 1), out list))
-                                list.Add((row, col));
-                            else
-                                links[(row, col + 1)] = new List<(int Row, int Col)>() { (row, col) };
-                        }
-                    }
-
-                    if (row + 1 < grid.Length && grid[row + 1][col] != 0)
-                    {
-                        if (links.TryGetValue((row, col), out var list))
-                        {
-                            list.Add((row + 1, col));
-                            if (links.TryGetValue((row + 1, col), out list))
-                                list.Add((row, col));
-                            else
-                                links[(row + 1, col)] = new List<(int Row, int Col)>() { (row, col) };
-                        }
-                        else
-                        {
-                            links[(row, col)] = new List<(int Row, int Col)>() { (row + 1, col) };
-                            if (links.TryGetValue((row + 1, col), out list))
-                                list.Add((row, col));
-                            else
-                                links[(row + 1, col)] = new List<(int Row, int Col)>() { (row, col) };
-                        }
-                    }
+            foreach (var rotter in rotters)
+            {
+                foreach(var potentialVictim in links[rotter])
+                {
+                    if (!rotters.Contains(potentialVictim))
+                        newVictims.Add(potentialVictim);
                 }
             }
 
-            var iterations = 0;
-            var stateChange = false;
-            do
+            stateChange = newVictims.Any();
+            if (newVictims.Any())
             {
-                stateChange = false;
-                var newVictims = new HashSet<(int Row, int Column)>();
+                rotters.UnionWith(newVictims);
+                fresh -= newVictims.Count;
+                iterations++;
+            }
+        } 
+        while (stateChange);
 
-                foreach (var rotter in rotters)
-                {
-                    foreach(var potentialVictim in links[rotter])
-                    {
-                        if (!rotters.Contains(potentialVictim))
-                            newVictims.Add(potentialVictim);
-                    }
-                }
+        if (fresh == 0)
+            return iterations;
 
-                stateChange = newVictims.Any();
-                if (newVictims.Any())
-                {
-                    rotters.UnionWith(newVictims);
-                    fresh -= newVictims.Count;
-                    iterations++;
-                }
-            } 
-            while (stateChange);
+        return -1;
+    }
 
-            if (fresh == 0)
-                return iterations;
+    [Test]
+    public void Test1()
+    {
+        Assert.That(OrangesRotting(
+                [
+                [2, 1, 1], 
+            [1, 1, 0], 
+        [0, 1, 1]
+            ]
+            ), Is.EqualTo(4));
+    }
 
-            return -1;
-        }
+    [Test]
+    public void Test2()
+    {
+        Assert.That(OrangesRotting([[2, 1, 1], [0, 1, 1], [1, 0, 1]]), Is.EqualTo(-1));
+    }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.That(OrangesRotting(
-                    [
-                    [2, 1, 1], 
-                    [1, 1, 0], 
-                    [0, 1, 1]
-                    ]
-                ), Is.EqualTo(4));
-        }
-
-        [Test]
-        public void Test2()
-        {
-            Assert.That(OrangesRotting([[2, 1, 1], [0, 1, 1], [1, 0, 1]]), Is.EqualTo(-1));
-        }
-
-        [Test]
-        public void Test3()
-        {
-            Assert.That(OrangesRotting([[0, 2]]), Is.EqualTo(0));
-        }
+    [Test]
+    public void Test3()
+    {
+        Assert.That(OrangesRotting([[0, 2]]), Is.EqualTo(0));
     }
 }
