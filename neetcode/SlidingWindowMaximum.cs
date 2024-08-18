d1a1b09b237cc35de5c80fb283c6250bb556cba4
Anthony Braund <abraund@users.noreply.github.com>
2024-08-18 18:44:54 +0100

    Shuffled it about until  it worked, the art of code.

---
+++ neetcode/SlidingWindowMaximum.cs
@@ -0,0 +1,44 @@
+namespace neetcode;
+
+public class SlidingWindowMaximum
+{
+    public int[] MaxSlidingWindow(int[] nums, int k)
+    {
+        var result = new int[nums.Length - k + 1];
+        var r = 0;
+        var l = () => r - k + 1;
+        var deque = new LinkedList<int>();
+
+        while (r < nums.Length)
+        {
+            while (deque.Any() && deque.Last() < nums[r])
+                deque.RemoveLast();
+
+            deque.AddLast(nums[r]);
+
+            if (r + 1 >= k)
+            {
+                result[l()] = deque.First();
+
+                if (deque.First() == nums[l()])
+                    deque.RemoveFirst();
+            }
+
+            r++;
+        }
+
+        return result;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal([2, 2, 4, 4, 6], MaxSlidingWindow([1, 2, 1, 0, 4, 2, 6], 3));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        Assert.Equal([1, -1], MaxSlidingWindow([1, -1], 1));
+    }
+}
