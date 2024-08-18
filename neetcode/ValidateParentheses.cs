10736c879b2454087df662ac4dffdec64d6696b4
Anthony Braund <abraund@users.noreply.github.com>
2024-08-18 19:03:04 +0100

    Checking if paretheses balance.

---
+++ neetcode/ValidateParentheses.cs
@@ -0,0 +1,53 @@
+using System.Linq;
+
+namespace neetcode;
+
+public class ValidatePartheses
+{
+    public bool IsValid(string s)
+    {
+        var brackets = new Dictionary<char, char>()
+        {
+            {'{', '}'},
+            {'[', ']'},
+            {'(', ')'},
+        };
+
+        var openStack = new Stack<char>();
+
+        foreach (var character in s)
+        {
+            if(brackets.ContainsKey(character))
+                openStack.Push(character);
+
+            if (brackets.ContainsValue(character))
+            {
+                if (!openStack.TryPop(out var openingChar) || brackets[openingChar] != character)
+                    return false;
+            }
+        }
+
+        if(openStack.Any())
+            return false;
+
+        return true;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(IsValid("[]"));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        Assert.False(IsValid("]"));
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        Assert.False(IsValid("["));
+    }
+}
