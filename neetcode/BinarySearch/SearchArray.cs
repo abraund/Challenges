57afcd6bdcaf56a6524950f3c8dd645524cc89d5
Anthony Braund <abraund@users.noreply.github.com>
2024-08-20 07:41:59 +0100

    Small update.

--- neetcode/BinarySearch/SearchArray.cs
+++ neetcode/BinarySearch/SearchArray.cs
@@ -13,10 +13,10 @@ public class SearchArraySolution
         while (l <= r)
         {
             var m = l + ((r - l) / 2);
-
-            if (nums[m] > target)
+            var found = nums[m];
+            if (found > target)
                 r = m - 1;
-            else if (nums[m] < target)
+            else if (found < target)
                 l = m + 1;
             else
                 return m;
