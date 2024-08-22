5836443ddeeb3a62ffd8f02bd82a8c9086ea19a3
Anthony Braund <abraund@users.noreply.github.com>
2024-08-22 23:17:51 +0100

    Tighter solution, notes.

--- neetcode/BinarySearch/FindMinimumInRotatedSortedArray.cs
+++ neetcode/BinarySearch/FindMinimumInRotatedSortedArray.cs
@@ -6,25 +6,22 @@ public class FindMinimumInRotatedSortedArray
     {
         var l = 0;
         var r = nums.Length - 1;
-        var result = int.MaxValue;
-
-        if (nums.Length == 1)
-            return nums[0];
 
         while (l < r)
         {
             var m = (l + r) / 2;
-            result = new int[] { result, nums[m], nums[r] }.Min();
 
-            if (nums[m] <= nums[l])
+            if (nums[l] > nums[m]) // down drop l->m
                 r = m;
-            else if (nums[m] >= nums[r])
+            if (nums[m] > nums[r]) // down drop r->m
                 l = m + 1;
-            else if (nums[l] < nums[r])
+            if (nums[l] < nums[r]) // solution
                 return nums[l];
+            /*else nums[m] == nums[r], doesn't seem to happen
+                r--;*/
         }
 
-        return result;
+        return nums[l];
     }
 
     [Fact]
@@ -73,5 +70,11 @@ public class FindMinimumInRotatedSortedArray
     public void Test8()
     {
         Assert.Equal(0, FindMin([4, 5, 6, 7, 0, 1, 2]));
+    }
+
+    [Fact]
+    public void Test9()
+    {
+        Assert.Equal(0, FindMin([4, 5, 6, 6, 6, 6, 6, 0, 1, 1, 1, 2]));
     }
 }
