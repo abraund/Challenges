1e3720ede2c9d8abac236be6068b1e3c43ac114d
Anthony Braund <abraund@users.noreply.github.com>
2024-08-27 19:51:47 +0100

    Interesting, to create a linked list the last value you want stored you need to enter first, and thus to reverse one simply rebuilds a new linked list.

---
+++ neetcode/LinkedList/ReverseALinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Neetcode.LinkedList;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+
+    public List<int> ToList()
+    {
+        var list = new List<int>() { val };
+        if(next != null)
+            list.AddRange(next.ToList());
+        return list;
+            
+    }
+}
+
+public class ReverseALinkedList
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        var nextNode = head;
+        var result = (ListNode)null;
+        while (nextNode != null)
+        {
+            result = new ListNode(nextNode.val, result);
+            nextNode = nextNode.next;
+        }
+
+        return result!;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var result = ReverseList(new ListNode(0, new ListNode(1, new ListNode(2, new ListNode(3)))));
+        Assert.Equal([3,2,1,0], result.ToList());
+    }
+}
